581849a1bd01ca178a73aef093e91917fb3a5c42
Paige Lieske <paigeproctor@comcast.net>
2019-06-24 14:29:26 -0400

    images are shuffling

--- src/App.js
+++ src/App.js
@@ -4,10 +4,44 @@ import Title from "./components/Title";
 import Wrapper from "./components/Wrapper";
 import pictures from "./pictures.json";
 
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
 class App extends Component {
   state = {
-    pictures
+    pictures,
+    clicked: [],
+    score: 0
   };
+
+  clickedPicture = id => {
+    let clicked = this.state.clicked;
+    let score = this.state.score;
+
+    if (clicked.indexOf(id) === -1) {
+      clicked.push(id);
+      this.setState({ score: this.state.score + 1 });
+      this.setState({ pictures: shuffle(pictures) });
+
+    } else if (this.state.score === 9) {
+      this.setState({
+        score: 0,
+        clicked: []
+      });
+    } else {
+      this.setState({
+        score: 0,
+        clicked: []
+      });
+
+    }
+  }
+
 
   render() {
     return (
@@ -18,11 +52,12 @@ class App extends Component {
             id={picture.id}
             key={picture.id}
             image={picture.image}
+            clickedPicture={this.clickedPicture}
           />
         ))}
       </Wrapper>
     );
-  }
+  };
 }
 
 export default App;
--- src/components/Picture/index.js
+++ src/components/Picture/index.js
@@ -1,12 +1,10 @@
 import React from "react";
 import "./style.css";
 
-function Picture(props) {
-    return (
-        <div className="img-container">
-            <img alt={props.name} src={props.image} />
-        </div>
-    );
-}
+const Picture = props => (
+    <div className="click-item" role="img" onClick={() => props.clickedPicture(props.id)}>
+        <img alt={props.name} src={props.image} />
+    </div>
+);
 
 export default Picture;
